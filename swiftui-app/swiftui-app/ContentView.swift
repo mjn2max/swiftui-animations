902a8f25a35ef38db583c59b67b44f8e90ddc662
Min Min <116730653+mjn2max@users.noreply.github.com>
2022-11-28 22:30:54 -0600

    Init project

---
+++ swiftui-app/swiftui-app/ContentView.swift
@@ -0,0 +1,29 @@
+//
+// ContentView.swift
+// swiftui-app
+//
+// Created by Huy D. on 11/27/22
+// duonghominhhuy.github.io 😜
+// 
+// Copyright © 2022 CodePassion.dev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-app/swiftui-app/swiftui_appApp.swift
@@ -0,0 +1,20 @@
+//
+// swiftui_appApp.swift
+// swiftui-app
+//
+// Created by Huy D. on 11/27/22
+// duonghominhhuy.github.io 😜
+// 
+// Copyright © 2022 CodePassion.dev. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
