c4446e42556207ca22fac6f278df4d549c7507ae
Huy D <116730653+mjn2max@users.noreply.github.com>
2024-10-13 23:13:54 -0500

    Update Package.swift

---
+++ Sources/ViewExtensions.swift
@@ -0,0 +1,39 @@
+//
+// SwiftUIView.swift
+// SwiftUI-Animations
+//
+// Created by Huy D. on 10/12/24
+// mjn2max.github.io 😜
+// 
+// Copyright © 2024. All rights reserved.
+// ___ORGANIZATIONNAME___
+//
+
+import SwiftUI
+
+@available(macOS 10.15, *)
+extension View {
+    public func hLeading() -> some View {
+        self.frame(maxWidth: .infinity, alignment: .leading)
+    }
+
+    public func hCenter() -> some View {
+        self.frame(maxWidth: .infinity, alignment: .center)
+    }
+
+    public func hTrailing() -> some View {
+        self.frame(maxWidth: .infinity, alignment: .trailing)
+    }
+
+    public func vTop() -> some View {
+        self.frame(maxHeight: .infinity, alignment: .top)
+    }
+
+    public func vCenter() -> some View {
+        self.frame(maxHeight: .infinity, alignment: .center)
+    }
+
+    public func vBottom() -> some View {
+        self.frame(maxHeight: .infinity, alignment: .bottom)
+    }
+}
