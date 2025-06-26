9d25b78b85fb57fe8dce7c55aebc47f6b2fab8c0
Huy D. <116730653+mjn2max@users.noreply.github.com>
2025-06-26 08:45:31 -0400

    Core+Animations | Add folder structure

---
+++ Sources/Core+Animations/FadeAnimations.swift
@@ -0,0 +1,18 @@
+//
+// FadeAnimations.swift
+// SwiftUI-Animations
+//
+// Created by Huy D. on 6/26/25
+// mjn2max.github.io 😜
+// 
+// Copyright © 2025. All rights reserved.
+// CodePassion.dev
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
