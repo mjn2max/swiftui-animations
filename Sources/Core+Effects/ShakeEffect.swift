f261748c91581e1bea8734f97a74d933ae081c7d
Huy D. <116730653+mjn2max@users.noreply.github.com>
2025-06-28 14:25:57 -0400

    Core+Effects | Add files

---
+++ Sources/Core+Effects/ShakeEffect.swift
@@ -0,0 +1,12 @@
+//
+// ShakeEffect.swift
+// SwiftUI-Animations
+//
+// Created by Huy D. on 6/26/25
+// mjn2max.github.io 😜
+// 
+// Copyright © 2025. All rights reserved.
+// CodePassion.dev
+//
+
+import Foundation
