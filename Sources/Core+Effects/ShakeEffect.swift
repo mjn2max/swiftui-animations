2a3bae5bb41d19b400c800c65f548fbf51ec74e8
Huy D. <116730653+mjn2max@users.noreply.github.com>
2025-08-30 12:47:14 -0400

    ShakeEffect | Add ShakeEffect struct

--- Sources/Core+Effects/ShakeEffect.swift
+++ Sources/Core+Effects/ShakeEffect.swift
@@ -4,9 +4,31 @@
 //
 // Created by Huy D. on 6/26/25
 // mjn2max.github.io 😜
-// 
+//
 // Copyright © 2025. All rights reserved.
 // CodePassion.dev
 //
 
-import Foundation
+import SwiftUI
+fileprivate struct ShakeEffect: GeometryEffect {
+    enum Axis {
+        case horizontal, vertical
+    }
+
+    var animatableData: CGFloat
+    var axis: Axis
+    var distance: CGFloat
+    var duration: Double
+
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        let translation: CGFloat
+        if axis == .horizontal {
+            translation = sin(animatableData * .pi * 2) * distance
+            return ProjectionTransform(CGAffineTransform(translationX: translation, y: 0))
+        } else {
+            translation = sin(animatableData * .pi * 2) * distance
+            return ProjectionTransform(CGAffineTransform(translationX: 0, y: translation))
+        }
+    }
+}
+
