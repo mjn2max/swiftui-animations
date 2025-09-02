4abe8ea69c0abc940199476e9443b3583caea762
Huy D. <116730653+mjn2max@users.noreply.github.com>
2025-09-01 21:31:49 -0400

    ShakeEffect | Add `.shakeHorizontal(distance: 10, duration: 0.1, repeats: 5)`

--- Sources/Core+Effects/ShakeEffect.swift
+++ Sources/Core+Effects/ShakeEffect.swift
@@ -10,6 +10,27 @@
 //
 
 import SwiftUI
+
+extension View {
+    /// A horizontal shake effect that moves the view left and right.
+    ///
+    /// - Parameters:
+    ///   - distance: The maximum horizontal offset.
+    ///   - duration: Duration of one shake cycle.
+    ///   - repeats: Number of repeats for the shake.
+    /// - Returns: A view with a horizontal shake animation.
+    ///
+    /// # Usage
+    /// ```swift
+    /// Text("Wrong Password")
+    ///     .shakeHorizontal(distance: 10, duration: 0.1, repeats: 5)
+    /// ```
+    func shakeHorizontal(distance: CGFloat = 10, duration: Double = 0.1, repeats: Int = 5) -> some View {
+        self.modifier(ShakeEffect(animatableData: CGFloat(repeats), axis: .horizontal, distance: distance, duration: duration))
+    }
+}
+
+
 fileprivate struct ShakeEffect: GeometryEffect {
     enum Axis {
         case horizontal, vertical
